e0615c2ad6d86eeaf200069be83e062f20f7914e
Michelli Cristina <contato@michellicristina.com.br>
2023-08-06 11:33:10 -0300

    First configurations

---
+++ todolist-swiftui/todolist_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolist_swiftuiApp.swift
+//  todolist-swiftui
+//
+//  Created by Michelli Cristina de Paulo Lima on 06/08/23.
+//
+
+import SwiftUI
+
+@main
+struct todolist_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ListView()
+        }
+    }
+}
